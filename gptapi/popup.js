708a62e56383034a86f3c58cc33a0379038627a8
Nate8888 <wilk.nathan@gmail.com>
2023-10-14 09:27:22 -0400

    assistant welcomes you + talks to you

--- gptapi/popup.js
+++ gptapi/popup.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    readTextWithElevenLabs("Hello, I am your personal assistant. How can I help you?");
     document.getElementById('sendButton').addEventListener('click', async function() {
         var inputField = document.getElementById('userInput');
         var question = inputField.value;
@@ -8,12 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
         try {
             let answer = await getAnswerFromGPT3(question);
             console.log('Bot Answer:', answer);
-    
             // Display the question and answer in the chatbox
             var chatbox = document.getElementById('chatbox');
             chatbox.innerHTML += "User: " + question + "<br>";
             chatbox.innerHTML += "Bot: " + answer + "<br>";
-            
+            await readTextWithElevenLabs(answer);
             // Clear the input field for the next question
             inputField.value = '';
         } 
@@ -23,6 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
  
     }, false);
 });
+
+async function readTextWithElevenLabs(msg) {
+    const url = "https://api.elevenlabs.io/v1/text-to-speech/ThT5KcBeYPX3keUQqHPh";
+
+    // Randomly select API key
+    let selectedKey = document.elevenKey;
+
+    const headers = {
+        "Accept": "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": selectedKey
+    };
+
+    const data = {
+        "text": msg,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+            "stability": 0.66,
+            "similarity_boost": 0.72,
+        }
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        });
+        const blob = await response.blob();
+        const audio = new Audio(URL.createObjectURL(blob));
+        audio.play();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 
 async function getAnswerFromGPT3(question){
     // Store the question 
