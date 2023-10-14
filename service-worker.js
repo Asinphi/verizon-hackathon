4e6192e004abfa4a3f03246c81e9bcbfe20165d9
rebeljah <rebeljah@gmail.com>
2023-10-14 02:37:58 -0400

    .

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
 // This is the service worker script, which executes in its own context
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
-
-import { runTranscriptionWorker } from "transcription/worker.js";
-
-runTranscriptionWorker();
