e6a15e54442cc3fce53b96274192e64f0b675766
rebeljah <rebeljah@gmail.com>
2023-10-14 12:51:57 -0400

    setup port communication for content scripts

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
 // This is the service worker script, which executes in its own context
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
+
+contentScriptPorts = {};
+
+chrome.runtime.onConnect.addListener(function(port) {
+    contentScriptPorts[port.name] = port;
+});
+
+contentScriptPorts["recorder"].postMessage("start");
+
+setTimeout(() => {contentScriptPorts["recorder"].stop()}, 5000);
---
+++ transcription/recorder.js
@@ -0,0 +1,24 @@
+
+var recorder = new webkitSpeechRecognition()
+recorder.continuous = true;
+recorder.interimResults = false;
+recorder.onstart = function() {
+    
+};
+recorder.onresult = function(event) {
+    transcript = '';
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        transcript += event.results[i][0].transcript;
+    }
+    console.log(transcript);
+};
+
+// setup connection to worker
+var port = chrome.runtime.connect({name: "recorder"});
+port.onMessage.addListener(function(msg) {
+    if (msg === "stop") {
+        recorder.stop();
+    } else if (msg === "start") {
+        recorder.start()
+    }
+});
