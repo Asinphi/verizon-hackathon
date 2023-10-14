dbe7972f88718c626f9b7e155c055dfb16c1157c
Asinphi <88jliu88@gmail.com>
2023-10-14 13:59:12 -0400

    refactor: separate chatpgt function
    Also changed how API key is accessed

--- gptapi/popup.js
+++ gptapi/popup.js
@@ -28,7 +28,7 @@ async function readTextWithElevenLabs(msg) {
     const url = "https://api.elevenlabs.io/v1/text-to-speech/ThT5KcBeYPX3keUQqHPh";
 
     // Randomly select API key
-    let selectedKey = document.elevenKey;
+    let selectedKey = elevenKey;
 
     const headers = {
         "Accept": "audio/mpeg",
@@ -60,51 +60,7 @@ async function readTextWithElevenLabs(msg) {
 }
 
 
-async function getAnswerFromGPT3(question){
-    // Store the question 
-    chrome.storage.sync.set({"question": question}, function() {
-        console.log("Question saved");
-    });
 
-    var url = 'https://api.openai.com/v1/chat/completions';
-    var options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + document.apiKey,
-        },
-        body: JSON.stringify({
-            'model': 'gpt-3.5-turbo',
-            'messages': [
-                {
-                    'role': 'system',
-                    'content': 'You are a chrome extension assistant helping the user with information'
-                },
-                {
-                    'role': 'user',
-                    'content': question
-                }
-            ]
-        })
-    };
-    
-    try {
-        var response = await fetch(url, options);
-        var data = await response.json();
-        var answer = data.choices[0].message.content;
-    
-        // Store the answer
-        chrome.storage.sync.set({"answer": answer}, function() {
-          console.log("Answer saved");
-        });
-             
-        return answer;
-    } 
-    catch(error){
-        console.error('Error:', error);
-        throw error;
-    }
-}
   
   window.onload = function() {
 
