ca36b7921f176a896c7bd3ddc77df00af407d076
Nate8888 <wilk.nathan@gmail.com>
2023-10-15 11:06:03 -0400

    hotfix

--- gptapi/chatgpt.js
+++ gptapi/chatgpt.js
@@ -270,6 +270,18 @@ async function summarizeAnswer(info, prompt){
 }
 
 async function pageMapper(prompt) {
+    const pageDescriptions = `
+Prepaid Plans: Choose the number of lines you are activating and then compare the different prices and plans to see what works for you. You can choose from the 15GB plan, the Unlimited plan and Unlimited Plus plan.
+
+Smartphones: If you don't have a smartphone yet, you can buy one from Verizon to use with the plan that you like. Select from different model smartphones to come with your prepaid plans, including Apple iPhones, Samsung Galaxies, and Google Pixels. Each model can be viewed from multiple angles and have their specifications compared with others.
+
+Apple iPhone 14: The user might choose an apple iPhone 14 Prepaid phone. You can choose a variety of colors and storage options and see the retail price for your selection, as well as images of various angles of the product.
+
+Accessories: After choosing which model (like the iPhone 14), you have the option of choosing accessories like cases, screen protectors, chargers, etc. to add to your orders.
+
+Cart: See all the items you currently have in your cart, some details about your products like color, storage, and price, and the plan you chose. Then when done, 'Begin secure checkout'
+
+Billing Information / Tell us About Yourself Form: On this page the user will input their personal information to finalize their order and billing information.`;
     const body = {
         "model": "gpt-3.5-turbo-0613",
         "messages": [
