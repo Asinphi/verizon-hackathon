faee51b845950dc30126311bec8ef63ec9e4ea41
Jarrod Humpel <rebeljah@gmail.com>
2023-10-15 04:41:47 -0400

    mascot on screen

--- natural-search.js
+++ natural-search.js
@@ -11,7 +11,7 @@ const container = document.createElement("div");
 container.classList.toggle("natural-search", true);
 document.body.appendChild(container);
 
-container.innerHTML = `<span class="natural-search__bot-msg"></span>
+container.innerHTML = `<canvas class="natural-search__mascot" id="mascotCanvas"></canvas><span class="natural-search__bot-msg"></span>
 <label for="natural-search__input" class="natural-search__input-label"></label>
 <textarea class="natural-search__input" id="natural-search__input" placeholder="What are you looking for?"></textarea>
 <input type="checkbox" style="display: none" id="natural-search__microphone-checkbox" class="natural-search__microphone-checkbox">
@@ -150,3 +150,204 @@ inputEl.addEventListener("keydown", async (e) => {
             recorder.stop();
     });
 }
+
+// Mascot Animation
+class BodyPart {
+    constructor(image, id, width, height) {
+        this.img = image;
+        this.id = id;
+        this.x = 0;
+        this.y = 0;
+        this.width = width;
+        this.height = height;
+        this.centerX = 0;
+        this.centerY = 0;
+        this.right = 0;
+        this.bottom = 0;
+        this.left = 0;
+        this.top = 0;
+
+        this.isBouncing = false;
+        this.yOffset = 0;
+        this.xOffset = 0;
+        this.sinBounceAngle = 0;
+        this.sinBounceHeight = 0;
+        this.sinBounceDelta = 0;
+    }
+  
+    draw(ctx) {
+        ctx.drawImage(this.img, this.origin().x + this.xOffset, this.origin().y + this.yOffset, this.width, this.height);
+    }
+  
+    setX(x) {
+        this.x = x;
+        this.left = x;
+        this.centerX = x + this.width / 2;
+        this.right = x + this.width;
+    }
+  
+    setLeft(x) {
+        this.setX(x);
+    }
+
+    setY(y) {
+        this.y = y;
+        this.top = y;
+        this.centerY = y + this.height / 2;
+        this.bottom = y + this.height;
+    }
+  
+    setTop(y) {
+        this.setY(y);
+    }
+
+    setCenterX(x) {
+        this.setX(x - this.width / 2);
+    }
+
+    setCenterY(y) {
+        this.setY(y - this.height / 2);
+    }
+
+    setRight(x) {
+        this.setX(x - this.width);
+    }
+
+    setBottom(y) {
+        this.setY(y - this.height);
+    }
+
+    startBouncing(height, sinDelta) {
+        this.isBouncing = true;
+        this.sinBounceHeight = height;
+        this.sinBounceDelta = sinDelta;
+    }
+
+    stopBouncing() {
+        this.isBouncing = false;
+    }
+
+    update() {
+        if (this.isBouncing) {
+            this.yOffset = -Math.sin(this.sinBounceAngle) * this.sinBounceHeight;
+            this.setY(this.origin().y + this.yOffset);
+            this.sinBounceAngle += this.sinBounceDelta;
+            if (this.sinBounceAngle >= Math.PI) {
+                this.sinBounceAngle = 0;
+            }
+        }
+
+        requestAnimationFrame(() => this.update());
+    }
+}
+
+
+class Mascot {
+    constructor(canvas, head, torso, handR, handL) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+
+        this.height = canvas.height;
+        this.width = canvas.width;
+        const h = this.height
+        console.log(canvas)
+
+        this.torso = new BodyPart(torso, "torso", .40*h, .49*h)
+        this.torso.setBottom(canvas.height);
+        this.torso.setCenterX(canvas.width/2);
+        this.head = new BodyPart(head, "head", .44*h, .42*h)
+        this.handR = new BodyPart(handR, "handR" ,.18*h,.18*h)
+        this.handL = new BodyPart(handL, "handL",.18*h,.18*h)
+
+        this.head.origin = () => {
+            return {x: this.torso.centerX - this.head.width/2 + .02*this.height, y: this.torso.top - this.head.height - .03*this.height};
+        }
+        this.handL.origin = () => {
+            return {x: this.torso.left - this.handL.width - .03*this.height, y: this.torso.centerY - .15*this.height};
+        }
+        this.handR.origin = () => {
+            return {x: this.torso.right + .03*this.height, y: this.torso.centerY - .15*this.height};
+        }
+        this.torso.origin = () => {
+            return {x: canvas.width/2 - this.torso.width/2, y: canvas.height - this.torso.height}
+        }
+
+        this.wavingHand = this.handR;
+
+        [this.torso, this.head, this.handL, this.handR].forEach((part) => {
+            part.update();
+            part.setX(part.origin().x);
+            part.setY(part.origin().y);
+        });
+        
+    }
+
+    startTalking() {
+        this.head.startBouncing(.008*this.height, Math.PI / 20);
+    }
+
+    stopTalking() {
+        this.head.stopBouncing();
+    }
+
+    startBouncing() {
+        this.torso.startBouncing(.01*this.height, Math.PI / 110);
+    }
+
+    stopBouncing() {
+        this.torso.stopBouncing();
+    }
+
+    startWaving() {
+        this.wavingHand.startBouncing(.10*this.height, Math.PI/40)
+    }
+    stopWaving() {
+        this.wavingHand.stopBouncing();
+        this.wavingHand = this.thisWavingHand === this.handL? this.handR : this.handL;
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.torso.draw(this.ctx);
+        this.head.draw(this.ctx);
+        this.handL.draw(this.ctx);
+        this.handR.draw(this.ctx);
+
+        requestAnimationFrame(() => this.draw());
+    }
+}
+
+function loadImage(fp) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.src = fp;
+
+        image.onload = () => {
+            resolve(image);
+        };
+
+        image.onerror = (error) => {
+            reject(error);
+        };
+    });
+}
+
+(async () => {
+    const canvas = document.getElementById("mascotCanvas")
+
+    const head = await loadImage(chrome.runtime.getURL("/assets/head.png"));
+    const torso = await loadImage(chrome.runtime.getURL("/assets/torso.png"));
+    const handL = await loadImage(chrome.runtime.getURL("/assets/hand.png"));
+    const handR = await loadImage(chrome.runtime.getURL("/assets/hand.png"));
+
+    const mascot = new Mascot(canvas, head, torso, handR, handL);
+    mascot.startTalking()
+    mascot.startWaving()
+    mascot.startBouncing()
+    setTimeout(() => {
+        mascot.stopWaving();
+        setTimeout(()=>mascot.startWaving(), 1000)
+    }, 2000)
+    mascot.draw();
+})();
